3cb998efc0059d6091de173e0a9e5523469b9365
Angelo Dalzotto <angelodalzotto97@gmail.com>
2020-10-30 10:58:14 -0300

    Performance optimization

--- bubblesort.c
+++ bubblesort.c
@@ -10,7 +10,7 @@
 
 unsigned last_pow_2(unsigned x);
 void bubblesort(int *array, const int SIZE);
-void interleave(int *src, int *dst, int length);
+void interleave(int *src_a, int len_a, int *src_b, int len_b, int *dst, int length);
 #if DEBUG == 1
 void print_array(int *array, int len);
 #endif
@@ -49,111 +49,133 @@ int main(int argc, char *argv[])
 	#endif
 #endif
 
-	/* Make the rank even */
-	unsigned evenrank = rank % 2 ? rank + 1 : rank;
-
-	/* Number of leaves in the tree depth */
-	unsigned depth_len = rank ? last_pow_2(evenrank) : 1;
-
-	/* Find the size of the array that will be computed in this node */
-	unsigned leaf_len = ROOT_LEN / depth_len;
-
-	/* Check if the node is the rightmost on its depth */
-	bool is_right_child = !(rank % 2) && rank != 0;
-
-	/* Find the parent of the leaf. This will be invalidated for root (node 0) */
-	int parent = evenrank / 2 - 1;
-
-	/* Find first child of the node. Will be invalidated if child >= size */
-	int child = (rank * 2) + 1;
-
-	/* Childs to the right can receive an extra value due to odd arrays */ 
-	int *values = malloc((leaf_len + is_right_child)*sizeof(int));
-	if(values == NULL){
-		printf("P%d: Not enough memory to allocate main array of length %d\n", rank, leaf_len + is_right_child);
-		MPI_Finalize();
-		exit(1);
-	}
-
 	MPI_Status status;
+	int *values = NULL;
+	int node_len;
+	int parent;
 
 #if DEBUG == 0
 	double then = 0;
 #endif
 	if(rank == 0){
 		/* Root: initial producer */
-		/* Populate array in decreasing order to execute worst case */
-	#if DEBUG == 1
-		printf("P%d: Populating main array\n", rank);
-	#endif
-		for(int i = 0; i < ROOT_LEN; i++)
-			values[i] = ROOT_LEN - i;
+		node_len = ROOT_LEN;
 
-	printf("P%d: Sorting array of %d elements in %d process\n", rank, ROOT_LEN, size);
-	#if DEBUG == 1
-		print_array(values, ROOT_LEN);
-	#else
-		then = MPI_Wtime();
-	#endif
-	} else {
-	#if DEBUG == 1
-		printf("P%d: Receiving array from parent %d\n", rank, parent);
-	#endif
-		/* Childs to the right can receive an extra value due to odd arrays */ 
-		MPI_Recv(values, leaf_len + is_right_child, MPI_INT, parent, 0, MPI_COMM_WORLD, &status);
-
-		/* Update this node number of elements */
-		MPI_Get_count(&status, MPI_INT, (int *)&leaf_len);
-	}
-
-	/* Check if number of elements in this node is odd */
-	bool is_odd = leaf_len % 2;
-
-	if(child + 1 < size){
-		/* Divide to childs */
-	#if DEBUG == 1
-		printf("P%d: Sending array to childs %d and %d\n", rank, child, child + 1);
-	#endif
-		MPI_Send(values, leaf_len / 2, MPI_INT, child, 0, MPI_COMM_WORLD);
-		MPI_Send(&values[leaf_len / 2], leaf_len / 2 + is_odd, MPI_INT, child + 1, 0, MPI_COMM_WORLD);
-
-		int *received = malloc(leaf_len*sizeof(int));
-		if(received == NULL){
-			printf("P%d: Not enough memory to allocate auxiliary array of length %d\n", rank, leaf_len);
+		/* Allocate memory for the whole array */
+		values = malloc(node_len*sizeof(int));
+		if(values == NULL){
+			printf("P%d: Not enough memory to allocate main array of length %d\n", rank, node_len);
 			MPI_Finalize();
 			exit(1);
 		}
 
-		/* Receive from one of the childs */
-		MPI_Recv(received, leaf_len / 2, MPI_INT, child, 0, MPI_COMM_WORLD, &status);
-		#if DEBUG == 1
-			printf("P%d: Received array from child %d\n", rank, status.MPI_SOURCE);
-			print_array(received, leaf_len / 2);
-		#endif
+		/* Populate array in decreasing order to execute worst case */
+	#if DEBUG == 1
+		printf("P%d: Populating main array\n", rank);
+	#endif
+		for(int i = 0; i < node_len; i++)
+			values[i] = node_len - i;
 
-		MPI_Recv(&received[leaf_len / 2], leaf_len / 2 + is_odd, MPI_INT, child + 1, 0, MPI_COMM_WORLD, &status);
-		#if DEBUG == 1
-			printf("P%d: Received array from child %d\n", rank, status.MPI_SOURCE);
-			print_array(&received[leaf_len / 2], leaf_len / 2 + is_odd);
-		#endif
+	printf("P%d: Sorting array of %d elements in %d process\n", rank, node_len, size);
+	#if DEBUG == 1
+		print_array(values, node_len);
+	#else
+		/* Measure time when not in DEBUG mode */
+		then = MPI_Wtime();
+	#endif
+
+	} else {
+		/* Check the size of the incoming array */
+		MPI_Probe(MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+		MPI_Get_count(&status, MPI_INT, &node_len);
+		parent = status.MPI_SOURCE;
+	#if DEBUG == 1
+		printf("P%d: Incoming array of size %d. Allocating memory\n", rank, node_len);
+	#endif
+
+		/* Allocate only the necessary memory */
+		values = malloc(node_len*sizeof(int));
+		if(values == NULL){
+			printf("P%d: Not enough memory to allocate main array of length %d\n", rank, node_len);
+			MPI_Finalize();
+			exit(1);
+		}
+
+	#if DEBUG == 1
+		printf("P%d: Receiving array from parent %d\n", rank, parent);
+	#endif
+		/* Finally receive from parent */ 
+		MPI_Recv(values, node_len, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+	}
+
+	/* Check the last child existance */
+	int first_child = rank*2 + 1;
+
+	if(first_child + 1 < size){
+		/* Divide to childs */
+	#if DEBUG == 1
+		printf("P%d: Sending array to childs %d and %d\n", rank, first_child, first_child + 1);
+	#endif
+		int half_len = node_len / 2;
+
+		/* No need to block the send. It will only receive the answer once sucessfuly sent */
+		MPI_Request request[2];
+		MPI_Isend(values, half_len, MPI_INT, first_child, 0, MPI_COMM_WORLD, &request[0]);
+		/* Send the remaining values (useful if node_len is odd) */
+		MPI_Isend(&values[half_len], node_len - half_len, MPI_INT, first_child + 1, 0, MPI_COMM_WORLD, &request[1]);
+
+		int *received[2];
+		received[0] = malloc(half_len*sizeof(int));
+		if(received[0] == NULL){
+			printf("P%d: Not enough memory to allocate auxiliary array of length %d\n", rank, half_len);
+			MPI_Finalize();
+			exit(1);
+		}
+
+		received[1] = malloc((node_len - half_len)*sizeof(int));
+		if(received[1] == NULL){
+			printf("P%d: Not enough memory to allocate auxiliary array of length %d\n", rank, node_len - half_len);
+			MPI_Finalize();
+			exit(1);
+		}
+
+		for(int i = 0; i < 2; i++){
+			/* Receive from one of the childs */
+			MPI_Probe(MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+			if(status.MPI_SOURCE == first_child){
+				MPI_Recv(received[0], half_len, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD, &status);
+			#if DEBUG == 1
+				printf("P%d: Received array from child %d\n", rank, status.MPI_SOURCE);		
+				print_array(received[0], half_len);
+			#endif
+			} else {
+				MPI_Recv(received[1], node_len - half_len, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD, &status);
+			#if DEBUG == 1
+				printf("P%d: Received array from child %d\n", rank, status.MPI_SOURCE);		
+				print_array(received[1], node_len - half_len);
+			#endif
+			}
+		}
 
 		/* Place back the message in the array (interleaved) */
-		interleave(received, values, leaf_len);
+		interleave(received[0], half_len, received[1], node_len - half_len, values, node_len);
 
-		free(received);
-		received = NULL;
+		for(int i = 0; i < 2; i++){
+			free(received[i]);
+			received[i] = NULL;
+		}
 	} else {
 		/* Conquer! */
 		#if DEBUG == 1
 			printf("P%d: Sorting array\n", rank);
 		#endif
-		bubblesort(values, leaf_len);
+		bubblesort(values, node_len);
 	}
 
 	if(rank == 0){
 	#if DEBUG == 1
 		/* Root node achieved. Show the array */
-		print_array(values, leaf_len);
+		print_array(values, node_len);
 	# else
 		double now = MPI_Wtime();
 		printf("P%d: Array sorted in %f\n", rank, now - then);
@@ -163,7 +185,8 @@ int main(int argc, char *argv[])
 			printf("P%d: Sending back to parent %d\n", rank, parent);
 		#endif
 		/* Send back array to parent */
-		MPI_Send(values, leaf_len, MPI_INT, parent, 0, MPI_COMM_WORLD);
+		/* This send can be blocking because there is no more computation after this */
+		MPI_Send(values, node_len, MPI_INT, parent, 0, MPI_COMM_WORLD);
 	}
 
 	free(values);
@@ -200,15 +223,15 @@ void bubblesort(int *array, const int SIZE)
 	}
 }
 
-void interleave(int *src, int *dst, int length)
+void interleave(int *src_a, int len_a, int *src_b, int len_b, int *dst, int length)
 {
 	int ia = 0;
-	int ib = length / 2;
+	int ib = 0;
 	for(int i = 0; i < length; i++){
-		if((ia < length / 2 && src[ia] <= src[ib]) || ib == length)
-			dst[i] = src[ia++];
+		if((ia < len_a && src_a[ia] <= src_b[ib]) || ib == len_b)
+			dst[i] = src_a[ia++];
 		else
-			dst[i] = src[ib++];
+			dst[i] = src_b[ib++];
 	}
 }
 
